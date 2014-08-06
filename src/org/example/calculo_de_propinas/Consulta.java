28ff321b5c1d61b7ce51b11a459ab9f9e9621282
ismaelAit <aitismaeld@gmail.com>
2014-08-06 15:52:29 -0300

    Primer versión con la aplicación vacía

---
+++ src/org/example/calculo_de_propinas/Consulta.java
@@ -0,0 +1,13 @@
+package org.example.calculo_de_propinas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Consulta extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_consulta);
+	}
+}
